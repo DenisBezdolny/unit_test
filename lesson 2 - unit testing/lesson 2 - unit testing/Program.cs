0af3e7c349f03bc586022a0422ed5f1fbf75eafe
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-03-16 15:05:53 +0300

    create unit testing project

---
+++ lesson 2 - unit testing/lesson 2 - unit testing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lesson_2___unit_testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
