dac74265731816738d7bfcd9085e8c9d9335498a
denis_bezdolny <d.i.d.c.o.d.a@gmail.com>
2024-03-16 15:15:51 +0300

    create Cat class

---
+++ lesson 2 - unit testing/lesson 2 - unit testing/Cat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson_2___unit_testing
+{
+    internal class Cat
+    {
+        public void Voice()
+        {
+            Console.WriteLine("Miau!!!");
+        }
+    }
+}
